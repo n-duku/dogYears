bc42eb41f561abc4e9862da358821b37255331e7
n-duku <ebenezer.duku@amalitech.org>
2021-08-10 11:33:51 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+//setting my age to a variable
+let myAge = 10;
+
+//variable to store early years
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+//Subtrafcting 2 from my age variable
+let laterYears = myAge - 2;
+
+//multiplying later years by 4
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+//total years in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+
+//storing my name in a variable and converting to lower case
+let myName = 'Duku'.toLowerCase()
+
+//Logging name, age, and age in dog years to console
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
+
+
